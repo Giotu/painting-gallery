4d696fa453dddab94a785d777aed033c37486b92
Giotu <evilalex1998@mail.ru>
2024-12-19 19:05:50 +0300

    feat: add search input for painting

--- src/components/PaintingList/PaintingList.vue
+++ src/components/PaintingList/PaintingList.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="painting-list">
-    <PaintingCard />
-    <PaintingCard />
-    <PaintingCard />
-    <PaintingCard />
-    <PaintingCard />
-    <PaintingCard />
-  </div>
+  <section class="painting">
+    <PaintingSearch />
+    <div class="painting-list">
+      <PaintingCard />
+      <PaintingCard />
+      <PaintingCard />
+      <PaintingCard />
+      <PaintingCard />
+      <PaintingCard />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
 import PaintingCard from "./PaintingCard.vue";
+import PaintingSearch from "./PaintingSearch.vue";
 </script>
 
 <style scoped lang="scss">
+.painting {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
 .painting-list {
   display: flex;
   justify-content: center;
